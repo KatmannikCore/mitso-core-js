05d95ad29253682f8e111ae5e8f709e7a6f3faff
KatmannikCore <vlad.urbanovitch@gmail.com>
2021-12-17 22:11:23 +0300

    fifty-seven-task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -556,8 +556,12 @@ function selectMany(arr, childrenSelector) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  indexes.map((curr, i) => {
+    // eslint-disable-next-line no-param-reassign
+    arr = arr[indexes[i]];
+  });
+  return arr;
 }
 
 /**
