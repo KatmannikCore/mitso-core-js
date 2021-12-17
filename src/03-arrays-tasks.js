10589e073048f8a0747c419a016c36d101eb9f5d
KatmannikCore <vlad.urbanovitch@gmail.com>
2021-12-17 22:08:03 +0300

    fifty-four-task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -482,8 +482,9 @@ function getIntervalArray(start, end) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  // eslint-disable-next-line no-shadow
+  return arr.filter((x, i, arr) => arr.indexOf(x) === i);
 }
 
 /**
